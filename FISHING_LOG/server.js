e2ad095a96625a0a4348e45a985a3dc81b8a3f24
Shannon Roberts <smrobertsllc@gmail.com>
2020-03-28 01:22:40 -0400

    readying code for heroku deployment

--- FISHING_LOG/server.js
+++ FISHING_LOG/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const fishes = require('.routes/api/fishes');
 
@@ -19,7 +20,17 @@ mongoose
     .catch(err => console.log(err));
 
 //Use Routes
-app.use('/api/fishes', fishes)  
+app.use('/api/fishes', fishes);
+
+//serve static assets if in production
+if(process.env.NODE_ENV === 'production') {
+    //set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 const port = process.env.PORT || 5000;
 
